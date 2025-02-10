05691a814792e6e9600e243de339b42768c8ac09
juhamikael <dev.juhamikael@gmail.com>
2025-02-10 02:03:17 +0200

    12.9 & 12.10

--- part12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
+++ part12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { Todo } = require('../mongo')
 const router = express.Router();
+const { getAsync, setAsync } = require('../redis');
+
+const COUNTER_KEY = 'added_todos';
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
@@ -10,12 +13,35 @@ router.get('/', async (_, res) => {
 
 /* POST todo to listing. */
 router.post('/', async (req, res) => {
-  const todo = await Todo.create({
-    text: req.body.text,
-    done: false
-  })
-  res.send(todo);
+  try {
+    const todo = await Todo.create({
+      text: req.body.text,
+      done: false
+    })
+    
+    let currentCount = await getAsync(COUNTER_KEY);
+    currentCount = currentCount ? Number(currentCount) + 1 : 1;
+    await setAsync(COUNTER_KEY, currentCount);
+
+    res.status(201).send(todo);
+  
+  } catch (error) {
+    console.error('Error creating todo:', error);
+    res.status(500).send('Error creating todo');
+  }
 });
+
+router.get('/statistics', async (req, res) => {
+  try {
+    let count = await getAsync(COUNTER_KEY);
+    count = count ? Number(count) : 0;
+    res.json({ added_todos: count });
+  } catch (error) {
+    console.error('Error getting statistics:', error);
+    res.status(500).send('Error getting statistics');
+  }
+});
+
 
 const singleRouter = express.Router();
 
